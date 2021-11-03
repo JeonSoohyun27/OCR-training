558b3efed3da28533991fa19aab3d59ea557941d
JeonSoohyun <jeonsoohyun27@gmail.com>
2021-11-03 15:52:38 +0900

    OCR test

---
+++ app.py
@@ -0,0 +1,11 @@
+
+import pytesseract
+
+from PIL import Image
+
+
+pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
+
+a = Image.open('영수증.jpg')
+result = pytesseract.image_to_string(a,lang='kor')
+print(result)
